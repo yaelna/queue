971ddfd023ccf62504a65150673b230f74efa01c
Barak Bar Orion <barak.bar@gmail.com>
2016-03-30 11:01:42 +0300

    Remove unused files. Add BlockingQueue interface.

---
+++ src/main/java/com/gigaspaces/queue/BlockingQueue.java
@@ -0,0 +1,13 @@
+package com.gigaspaces.queue;
+
+/**
+ * Created by Barak Bar Orion
+ * on 3/30/16.
+ *
+ * @since 11.0
+ */
+@SuppressWarnings("unused")
+interface BlockingQueue<T> {
+    T get() throws InterruptedException;
+    void put(T t) throws InterruptedException;
+}
